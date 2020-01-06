68e5fc1a873d85a30b0a473bbc5379bc52b1756b
Jason Young <jyoung8607@gmail.com>
2020-01-06 03:15:18 -0500

    Fix unused import

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -2,7 +2,7 @@ import os
 from common.params import Params
 from common.basedir import BASEDIR
 from selfdrive.car.fingerprints import eliminate_incompatible_cars, all_known_cars
-from selfdrive.car.vin import get_vin, VIN_UNKNOWN
+from selfdrive.car.vin import get_vin
 from selfdrive.swaglog import cloudlog
 import cereal.messaging as messaging
 from selfdrive.car import gen_empty_fingerprint
