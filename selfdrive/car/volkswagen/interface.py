06118157c8e88e9c7f7bf5c962db5c78a751836a
jpheur <jpheur@gmail.com>
2020-01-20 15:40:13 +0200

    Removed speed based parameters

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -70,8 +70,8 @@ class CarInterface(CarInterfaceBase):
       # HCA assist torque, but if they're good breakpoints for the driver,
       # they're probably good breakpoints for HCA as well. OP won't be driving
       # 250kph/155mph but it provides interpolation scaling above 100kmh/62mph.
-      ret.lateralTuning.pid.kpBP = [0., 15 * CV.KPH_TO_MS, 50 * CV.KPH_TO_MS, 100 * CV.KPH_TO_MS, 250 * CV.KPH_TO_MS]
-      ret.lateralTuning.pid.kiBP = [0., 15 * CV.KPH_TO_MS, 50 * CV.KPH_TO_MS, 100 * CV.KPH_TO_MS, 250 * CV.KPH_TO_MS]
+      ret.lateralTuning.pid.kpBP = [0.]
+      ret.lateralTuning.pid.kiBP = [0.]
 
       # FIXME: Per-vehicle parameters need to be reintegrated.
       # For the time being, per-vehicle stuff is being archived since we
@@ -87,8 +87,8 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.45
       ret.steerRatio = 15.6
       ret.lateralTuning.pid.kf = 0.00006
-      ret.lateralTuning.pid.kpV = [0.15, 0.25, 0.35, 0.45, 0.45]
-      ret.lateralTuning.pid.kiV = [0.05, 0.05, 0.05, 0.05, 0.05]
+      ret.lateralTuning.pid.kpV = [0.25]
+      ret.lateralTuning.pid.kiV = [0.05]
       tire_stiffness_factor = 1.0
 
     ret.enableCamera = True  # Stock camera detection doesn't apply to VW
