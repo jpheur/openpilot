70aeaea810bfc82013d441f9ed398d0828d8566f
jpheur <jpheur@gmail.com>
2020-01-23 18:33:51 +0200

    Updated crash upload url

--- selfdrive/crash.py
+++ selfdrive/crash.py
@@ -19,7 +19,7 @@ if os.getenv("NOLOG") or os.getenv("NOCRASH"):
 else:
   from raven import Client
   from raven.transport.http import HTTPTransport
-  client = Client('https://1994756b5e6f41cf939a4c65de45f4f2:cefebaf3a8aa40d182609785f7189bd7@app.getsentry.com/77924',
+  client = Client('https://455d5ed33ac54d849b9441abebe528f6:1c6c4021233b4f748266b4bb3ee83f5c@sentry.io/1887402',
                   install_sys_hook=False, transport=HTTPTransport, release=version, tags={'dirty': dirty})
 
   def capture_exception(*args, **kwargs):
